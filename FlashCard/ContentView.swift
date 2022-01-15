351268690efd707ac4a7c581c477504d5f184f0e
Daichi Morihara <daichi16daichi04@icloud.com>
2022-01-15 18:22:51 +0900

    Initial Commit

---
+++ FlashCard/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  FlashCard
+//
+//  Created by Daichi Morihara on 2022/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+    
+    var body: some View {
+        let dragGesture = DragGesture()
+            .onChanged { value in offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+              }
+          }
+        let pressGesture = LongPressGesture()
+                .onEnded { value in
+                    withAnimation {
+                        isDragging = true
+              }
+          }
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.red)
+            .frame(width: 50, height: 50)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlashCard/FlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardApp.swift
+//  FlashCard
+//
+//  Created by Daichi Morihara on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
