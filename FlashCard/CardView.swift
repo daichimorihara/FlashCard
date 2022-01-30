72ff3603be868df12ee71ac1b422bbe7802f205c
Daichi Morihara <daichi16daichi04@icloud.com>
2022-01-30 15:22:43 +0900

    polish UI/UX little bit

--- FlashCard/CardView.swift
+++ FlashCard/CardView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)?
     @State private var showingAnswer = false
+    @State private var offset = CGSize.zero
+    @State private var feedback = UINotificationFeedbackGenerator()
+    
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .fill(.white)
+                .fill(
+                    .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             VStack {
                 Text(card.prompt)
@@ -29,9 +41,34 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(dragGesture)
         .onTapGesture {
             showingAnswer.toggle()
         }
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { gesture in
+                offset = gesture.translation
+                feedback.prepare()
+            }
+            .onEnded { _ in
+                if abs(offset.width) > 100 {
+                    // remove card
+                    if offset.width > 0 {
+                        feedback.notificationOccurred(.success)
+                    } else {
+                        feedback.notificationOccurred(.error)
+                    }
+                    removal?()
+                } else {
+                    offset = .zero
+                }
+            }
     }
 }
 
--- FlashCard/ContentView.swift
+++ FlashCard/ContentView.swift
@@ -8,16 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = [Card]()
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    @State private var showingEditScreen = false
     
     
     var body: some View {
-        VStack {
-            ZStack {
-                // place cardview
+        NavigationView {
+            VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.7))
+                    .clipShape(Capsule())
+                
+                ZStack {
+                    // place cardview
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
+                    }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.black.opacity(0.7))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+                
+            }
+            .sheet(isPresented: $showingEditScreen, onDismiss: resetCards) {
+                EditCard()
+            }
+            .onAppear(perform: resetCards)
+            .padding(.bottom, 100)
+            .navigationBarItems(trailing: Button("Edit") {
+                showingEditScreen = true
+            })
+            .onReceive(timer) { time in
+                guard isActive else { return }
+                
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                }
+            }
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase == .active {
+                    if cards.isEmpty == false {
+                        isActive = true
+                    }
+                } else {
+                    isActive = false
+                }
+            }
+        }
+    }
+
+    
+    func removeCard(at index: Int) {
+        guard index >= 0 else { return }
+        
+        cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        timeRemaining = 100
+        isActive = true
+        loadData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
             }
         }
     }
 }
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
